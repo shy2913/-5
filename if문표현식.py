7a3537505cca0fa0b274b9778485376817c283b1
DESKTOP-PKK3F39\admin <yohan0355@daum.net>
2021-04-28 20:47:53 +0900

    python practice 5

---
+++ if문표현식.py
@@ -0,0 +1,6 @@
+# 뭔가 반복적으로 엄청 수행해야 한다!
+# 반복문(while문)
+score = 70
+message = "success" if score >= 60 else "failure"
+# 3항 연산자
+print(message)
---
+++ py11.py
@@ -0,0 +1,11 @@
+pocket = ['paper', 'cellphone']
+card = False
+a = False
+if 'meney' in pocket:
+    pass
+elif card:
+    print("택시를 타고 가라")
+elif a:
+    print("aa")
+else:
+    print("걸어가라")
